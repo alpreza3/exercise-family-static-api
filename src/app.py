3a7622ffb498aea89e77d2db4be0005bb6253edf
Albert Pregonas <albertpregonas@gmail.com>
2025-04-07 08:07:02 +0000

    second commit

--- src/app.py
+++ src/app.py
@@ -15,6 +15,25 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+jackson_family.add_member({
+    "id": 1,
+    "first_name": "John",
+    "age": 33,
+    "lucky_numbers": [7, 13, 22]
+})
+jackson_family.add_member({
+    "id": 2,
+    "first_name": "Jane",
+    "age": 35,
+    "lucky_numbers": [10, 14, 3]
+})
+jackson_family.add_member({
+    "id": 3,
+    "first_name": "Jimmy",
+    "age": 5,
+    "lucky_numbers": [1]
+})
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -27,13 +46,8 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def get_members():
-    try:
-        members = jackson_family.get_all_members()
-        return jsonify(members), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    return jsonify(jackson_family.get_all_members()), 200
 
-@app.route('/member/<int:member_id>', methods=['GET'])
 def get_member(member_id):
     member = jackson_family.get_member(member_id)
     if member:
@@ -42,22 +56,17 @@ def get_member(member_id):
 
 @app.route('/member', methods=['POST'])
 def add_member():
-    try:
-        data = request.get_json()
-        if not data or "first_name" not in data or "age" not in data or "lucky_numbers" not in data:
-            return jsonify({"error": "Missing required member fields"}), 400
-        jackson_family.add_member(data)
-        return jsonify({"msg": "Member added successfully"}), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    data = request.get_json()
+    jackson_family.add_member(data)
+    return jsonify({"member": data}), 200
 
 @app.route('/member/<int:member_id>', methods=['DELETE'])
 def delete_member(member_id):
     member = jackson_family.get_member(member_id)
-    if not member:
-        return jsonify({"error": "Member not found"}), 400
-    jackson_family.delete_member(member_id)
-    return jsonify({"done": True}), 200
+    if member:
+        jackson_family.delete_member(member_id)
+        return jsonify({"done": True}), 200
+    return jsonify({"error": "Member not found"}), 400
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -11,31 +11,7 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
-        # example list of members
-        self._members = [
-            {
-                "id": self._generateId(),
-                "first_name": "John",
-                "last_name": self.last_name,
-                "age": 33,
-                "lucky_numbers": [7, 13, 22]
-            },
-            {
-                "id": self._generateId(),
-                "first_name": "Jane",
-                "last_name": self.last_name,
-                "age": 35,
-                "lucky_numbers": [10, 14, 3]
-            },
-            {
-                "id": self._generateId(),
-                "first_name": "Jimmy",
-                "last_name": self.last_name,
-                "age": 5,
-                "lucky_numbers": [1]
-            }
-        ]
+        self._members = []
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -44,9 +20,9 @@ class FamilyStructure:
     def add_member(self, member):
         if "id" not in member:
             member["id"] = self._generateId()
-        member["last_name"] = self.last_name
+        if "last_name" not in member:
+            member["last_name"] = self.last_name
         self._members.append(member)
-        pass
 
     def delete_member(self, id):
         self._members = [m for m in self._members if m["id"] != id]
